560907f926c34bee19acc132fa883608aa183866
marikf98 <marikf98@gmail.com>
2023-05-21 11:16:06 +0300

    123

--- Hackathon/Form1.cs
+++ Hackathon/Form1.cs
@@ -16,5 +16,36 @@ namespace Hackathon
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startSimulationButton_Click(object sender, EventArgs e)
+        {
+            // Retrieve the input values
+            int numberOfProducers = (int)numberOfProducersNumericUpDown.Value;
+            int numberOfConsumers = (int)numberOfConsumersNumericUpDown.Value;
+            int producerRate = (int)producerRateNumericUpDown.Value;
+            int consumerRate = (int)consumerRateNumericUpDown.Value;
+
+            // Validate the input values (you can add your own validation logic)
+            if (numberOfProducers <= 0 || numberOfConsumers <= 0 || producerRate <= 0 || consumerRate <= 0)
+            {
+                MessageBox.Show("Please enter valid simulation data.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Pass the simulation data to the next screen or initiate the simulation
+            // You can open the next screen or start the simulation based on your implementation
+            // For example, you can create a new instance of the simulation form and pass the data to it
+            
+
+            // Close the current input form (optional)
+            this.Hide();
+        }
+
+
     }
 }
